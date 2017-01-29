4701c6bf8da85f03b487349ad41f076b8d684ec5
jyepesr1 <johanyepes@hotmail.com>
2017-01-29 13:53:39 -0500

    Changing Database

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
-SQLALCHEMY_DATABASE_URI = 'mysql://root:jonNBZ91@mysql:3306/projman_db'
+#SQLALCHEMY_DATABASE_URI = 'mysql://root:jonNBZ91@mysql:3306/projman_db'
+SQLALCHEMY_DATABASE_URI = 'mysql://bfa6dae0546e5c:c3699b2c@us-cdbr-iron-east-04.cleardb.net/heroku_1fd38b1f3e312ef'

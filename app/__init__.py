bcbbafb14f1d1843cade2e6cadbbdd8cecd51aed
jyepesr1 <johanyepes@hotmail.com>
2017-01-29 15:08:36 -0500

    Fixing

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__, instance_relative_config=True)
 
 #Include config from config.py
-app.config.from_object('config')
+#app.config.from_object('config')
 app.config.from_pyfile('config.py')
 
 #Create an instance of SQLAclhemy

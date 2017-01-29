d7d106c890d70c80951ee3cbc186d9d3bebd3ee0
jyepesr1 <johanyepes@hotmail.com>
2017-01-29 15:10:45 -0500

    Adding Instance Folder

---
+++ instance/config.py
@@ -0,0 +1,3 @@
+SECRET_key = '\xd3Q\x97\nw\x94n\x0cW1\x1a:\xed\x0b\x0e\x00G\xd3 4\xe7k\nY'
+SQLALCHEMY_DATABASE_URI = 'mysql://bfa6dae0546e5c:c3699b2c@us-cdbr-iron-east-04.cleardb.net/heroku_1fd38b1f3e312ef'
+

56d01b2d8d52f29f1aade75f4c60e7df754c451e
jyepesr1 <johanyepes@hotmail.com>
2017-01-29 15:06:26 -0500

    Creating gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 #Create an Instance of Flask
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 
 #Include config from config.py
 app.config.from_object('config')
-app.secret_key = 'dfdffvfdvfd441445!=sdc'
+app.config.from_pyfile('config.py')
 
 #Create an instance of SQLAclhemy
 db = SQLAlchemy(app)
